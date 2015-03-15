6fb53f59cc09510f362fe674b9f8b97e629e9d91
Guido García <guido@tid.es>
2015-03-15 18:59:21 +0100

    marathon api env var

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var request = require('request'),
     Firebase = require('firebase'),
     Marathon = require('marathon.node');
 
-// TODO read from consul
-var MARATHON_API = 'http://10.141.141.10:8080';
-var CONSUL_NODE = '81.45.22.27:8500';
+// TODO read config from consul
+var MARATHON_API = process.env.MARATHON_API || 'http://10.141.141.10:8080';
+var CONSUL_NODE = process.env.CONSUL_NODE || '81.45.22.27:8500';
 var DEFAULT_BRANCH = 'develop'; // TODO get branch from the commit information
-var FIREBASE_ENDPOINT = 'https://flock-demo.firebaseio.com/'
+
+// XXX using firebase only because it is a simple pubsub mechanism already integrated with github
+var FIREBASE_ENDPOINT = process.env.FIREBASE_ENDPOINT || 'https://flock-demo.firebaseio.com/'
 
 var root = new Firebase(FIREBASE_ENDPOINT);
 root.limitToLast(1).on('child_added', function onchange(child) {
@@ -48,7 +50,7 @@ function getPackage(commit, callback) {
     var packageUrl = 'https://raw.githubusercontent.com/' + login + '/' + repository + '/' + DEFAULT_BRANCH + '/package.json';
     console.log('download package.json from %s', packageUrl)
 
-    request.get({url:packageUrl, json:true}, function (err, response, package) {
+    request.get({url:packageUrl, json:true}, function onPackage(err, response, package) {
         // TODO handle error
         callback(err, package);
     })
@@ -57,7 +59,7 @@ function getPackage(commit, callback) {
 function buildApp(package) {
     var moduleUrl = package.repository.url.replace(/git@(.*):/, 'git://$1/') + '#' + DEFAULT_BRANCH;
 
-    // TODO move to a custom docker image (flock-docker)
+    // TODO move to a custom docker image (see flock-docker)
     var INSTALL_ENVCONSUL = 'cd /tmp && wget https://github.com/hashicorp/envconsul/releases/download/v0.3.0/envconsul_0.3.0_linux_amd64.tar.gz && tar xfz envconsul_0.3.0_linux_amd64.tar.gz && mv /tmp/envconsul_0.3.0_linux_amd64/envconsul /usr/local/bin && chmod +x /usr/local/bin/envconsul && rm -rf /tmp/envconsul*';
     var RUN_APP = 'npm install ' + moduleUrl + ' && cd node_modules && cd ' + package.name + ' && envconsul -consul=' + CONSUL_NODE + ' service/' + package.name + ' node server';
 
